0e52b44325fc4f5c7cca840bf2d0469d571e3e79
Ryan-Zayne <phoenixastro97@gmail.com>
2025-09-28 00:24:37 +0100

    chore: remove astro font loader

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,8 @@
-import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
-	experimental: {
-		fonts: [
-			{
-				cssVariable: "--font-sans",
-				name: "Inter",
-				provider: fontProviders.google(),
-				weights: [400, 500, 600, 700, 900],
-			},
-			{
-				cssVariable: "--font-heading",
-				name: "Bricolage Grotesque",
-				provider: fontProviders.google(),
-				weights: [400, 500, 600, 700, 900],
-			},
-		],
-	},
-
 	integrations: [react()],
 
 	vite: {
